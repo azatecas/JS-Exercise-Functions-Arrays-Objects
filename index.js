314b9b60220e4686dd27d298417819235f9dfe96
Luis PM <luisp237@gmail.com>
2019-12-16 16:31:13 -0500

    makeSmartPerson completed

--- index.js
+++ index.js
@@ -80,8 +80,18 @@ function getName(testObject) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(namePassed) {
+  const answer = {
+    name: namePassed,
+    sum: function(num1,num2){
+      return num1 + num2;
+    },
+    speak: function(){
+      return 'Hello, my name is ' + namePassed;
+    }
+  };
+
+  return answer;
 }
 
 
